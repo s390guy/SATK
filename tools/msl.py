f5477960a09edc755c8c5aee9dfcdd030e0feefb
Harold Grovesteen <h.grovsteen@tx.rr.com>
2014-02-13 13:23:06 -0600

    Add command line wrapper for Machine Specification Language (MSL) Database tool - msl.py

---
+++ tools/msl.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python3.3
+# Copyright (C) 2014 Harold Grovesteen
+#
+# This file is part of SATK.
+#
+#     SATK is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     SATK is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with SATK.  If not, see <http://www.gnu.org/licenses/>.
+
+# This module provides a command line interface to the Machine Specification Language
+# (MSL) database generted by the msldb module.  For a description of the MSL, see
+# module msldb.py.
+
+# PYthon imports:
+import argparse          # Access the command line parser
+import sys               # Access the exit method
+
+# ASMA imports
+import msldb             # Access the database
+
+copyright="msl.py Copyright (C) %s Harold Grovesteen" % "2014"
+
+
+#
+#  +----------------------------+
+#  |                            |
+#  |   Command Line Execution   |
+#  |                            |
+#  +----------------------------+
+#
+
+class MSLIF(object):
+    def __init__(self,args):
+        self.mslfile=args.mslfile[0]   # Input MSL text file
+
+        # Normal processing options
+        self.expand=args.expand        # Expand a cpu statment with dependencies
+        self.fail=args.fail            # If True fail immediately on an error
+        self.xref=args.xref            # Print the cross-reference listing
+
+        # Diagnostic options
+        self.dump=args.dump            # Dump the final output DB
+
+        # The Machine Language Specification Processor
+        self.dbp=msldb.MSL()
+
+    def run(self):
+        keep=self.expand is not None
+        errors=self.dbp.build(self.mslfile,xref=self.xref,fail=self.fail,keep=keep)
+        self.dbp.printWarnings()
+        if errors:
+            self.dbp.printErrors()
+            if self.expand is not None:
+                cls_str="msl.py - %s.run() -" % self.__class__.__name__ 
+                print("%s output suppressed due to database build errors" % cls_str)
+            sys.exit(2)
+        if self.xref:
+            self.dbp.xref()
+        final=self.dbp.DB()
+        if self.expand is not None:
+            final=self.dbp.expand(self.expand)
+            # Final may be a MSLDB object or a CPUX object
+        if self.dump is not None:
+            if self.dump=="full":
+                final.dump(sort=True)
+            else:
+                final.summarize(sort=True)
+
+#
+#  +-----------------------------+
+#  |                             |
+#  |   Command Line Processing   |
+#  |                             |
+#  +-----------------------------+
+#
+
+# Parse the command line arguments
+def parse_args():
+    parser=argparse.ArgumentParser(prog="msl.py",
+        epilog=copyright, 
+        description="validates for correctness a Machine Specification Language "
+                    "text file")
+
+    # Input text file (attribute mslfile in the parse_arg namespace will be a list)
+    parser.add_argument("mslfile",\
+        help="input MSL source file path",nargs=1)
+
+    # Extract the machine specification for a specific machine, or 'all', the default.
+    parser.add_argument("-e","--expand",\
+        help="expand this cpu statemnt with database subset")
+
+    # Force failure upon detecting an error
+    parser.add_argument("-f","--fail",action="store_true",default=False,\
+        help="fail upon detection of an error, otherwise report at process end")
+
+    # Force failure upon detecting an error
+    parser.add_argument("-x","--xref",action="store_true",default=False,\
+        help="produce a statement cross reference listing")
+
+    # Force failure upon detecting an error
+    parser.add_argument("--dump",choices=["full","summary"],\
+        help="dumps the the database entries' Python content")
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args=parse_args()
+    print(copyright)
+    MSLIF(args).run()
